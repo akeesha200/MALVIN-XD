a24af5ede560bbd68df00ba195644c98882702b7
akeesha200 <Shankam342@gmail.com>
2025-02-26 07:38:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~RlA0VZ5R#lE9N6IIYdWbLfm_iDWAb7n6AVsIzECPupqwyRThxQKk",
 // add your Session Id 
 
 PREFIX: process.env.PREFIX || ".",
